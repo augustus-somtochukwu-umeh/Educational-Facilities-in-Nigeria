0dd128973b94cf4f6d3aa10a05a4b6eb033e6fc4
augustus-somtochukwu-umeh <umehaugustus@gmail.com>
2026-03-05 18:16:25 +0100

    initial commit

--- Education.app.py
+++ Education.app.py
@@ -44,52 +44,88 @@ def main():
     if Lga_filter:
         filtered_data=filtered_data[filtered_data['Unique_lga'].isin (Lga_filter)]
 
-    st.title("🏫📚Educational Infrastructure and Access Dashboard")
-    st.markdown("---")
-    def format_number(num):
-        if num >= 1_000_000_000:
-            return f"{num/1_000_000_000:.2f}B"
-        elif num >= 1_000_000:
+        st.title("🏫📚Educational Infrastructure and Access Dashboard")
+        st.markdown("---")
+        def format_number(num):
+          if num >= 1_000_000_000:
+               return f"{num/1_000_000_000:.2f}B"
+          elif num >= 1_000_000:
             return f"{num/1_000_000:.2f}M"
-        elif num >= 1_000:
+          elif num >= 1_000:
             return f"{round(num/1_000)}K"
-        else:
+          else:
             return str(num)
     
-    col1, col2, col3 = st.columns(3)
-    with col1:
-         Total_schools = len(filtered_data)
-         st.metric("🏫Total Schools", format_number(Total_schools))
-    with col2:
-         Total_students = filtered_data['Total_students'].sum()
-         st.metric("👨‍🎓👩‍🎓Total Students", format_number(Total_students))
-    with col3:
-         Average_student=filtered_data['Total_students'].mean()
-         st.metric("📊Average Student per Schoool", f"{Average_student:.2f}")
-    col4, col5= st.columns(2)
-    with col4:
-         Electricity_access=filtered_data['Electricity_access'].mean()*100
-         st.metric("⚡Percentage of Schools with Electricity Access", f"{Electricity_access:.2f}%")
-    with col5:
-         Improved_water_supply=filtered_data['Improved_water_supply'].mean()*100
-         st.metric("🚰Percentage of schools with Improved Water Supply", f"{Improved_water_supply:.2f}%")
+        col1, col2, col3 = st.columns(3)
+        with col1:
+          Total_schools = len(filtered_data)
+          st.metric("🏫Total Schools", format_number(Total_schools))
+        with col2:
+          Total_students = filtered_data['Total_students'].sum()
+          st.metric("👨‍🎓👩‍🎓Total Students", format_number(Total_students))
+        with col3:
+          Average_student=filtered_data['Total_students'].mean()
+          st.metric("📊Average Student per Schoool", f"{Average_student:.2f}")
+        
+        col4, col5= st.columns(2)
+        with col4:
+          Electricity_access=filtered_data['Electricity_access'].mean()*100
+          st.metric("⚡Percentage of Schools with Electricity Access", f"{Electricity_access:.2f}%")
+        with col5:
+          Improved_water_supply=filtered_data['Improved_water_supply'].mean()*100
+          st.metric("🚰Percentage of schools with Improved Water Supply", f"{Improved_water_supply:.2f}%")
     
     st.markdown("---")
     col1, col2 = st.columns(2)
     with col1:
-         st.subheader("Distribution of School Types")
-         school_type= filtered_data['School_type'].value_counts().reset_index()
-         school_type.columns= ['School_type', 'counts']
-         fig_school_type=px.bar(
-              school_type,
-              x='School_type',
-              y='counts',
-              color='School_type',
-              color_continuous_scale=["#1f77b4"] 
-
-         )
-         fig_school_type.update_layout(width=1000, height=500)
-         st.plotly_chart(fig_school_type, use_container_width=True)
+       st.subheader("Distribution of School Types")
+       school_type= filtered_data['School_type'].value_counts().reset_index().head(5)
+       school_type.columns= ['School_type', 'counts']
+       fig_school_type=px.bar(
+          school_type,
+          x='School_type',
+          y='counts',
+          height=500,
+          color='School_type',
+          color_continuous_scale="Blues" 
+       )
+       fig_school_type.update_layout(height=500)
+       st.plotly_chart(fig_school_type, use_container_width=True)
+     
+    with col2:
+     st.subheader("Student Population Distribution")
+     fig_students = px.histogram(
+        filtered_data,
+        x="Total_students",
+        nbins=50   
+     )
+     fig_students.update_layout(height=500)
+     st.plotly_chart(fig_students, use_container_width=True)
+     
+     col1, col2=st.columns(2)
+     with col1:
+        st.subheader("Comparison of Management Type")
+        management_type_comparison=filtered_data.groupby('Management_type')['Total_students'].sum().reset_index()
+        management_type_comparison = management_type_comparison.sort_values(by='Total_students')
+        fig_management_comparison=px.bar(
+           management_type_comparison,
+           x='Management_type',
+           y='Total_students',
+           height=600,
+           color="Total_students",
+           color_continuous_scale='viridis'
+        )
+        fig_management_comparison.update_layout(
+           height=600,
+           title='Total Students by Management Type',
+           yaxis_title='Total_students',
+           xaxis_title='Management_type',
+           title_x=0.5
+        )
+        st.plotly_chart(fig_management_comparison, use_container_width=True)
+        
+        
+          
 
 
 if __name__ == "__main__":
